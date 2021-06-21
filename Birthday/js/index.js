40fb67b3070a8780cac51695e975ec5133ebebac
Saketh <72593220+Saketh-debug@users.noreply.github.com>
2021-06-21 11:51:34 +0530

    Update index.js

--- Birthday/js/index.js
+++ Birthday/js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "Birthday/My Video.mp4");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/btlxjt83bCo?controls=0&loop=1&autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
